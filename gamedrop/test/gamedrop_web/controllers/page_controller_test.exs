8bde05fa98713dee53d06efa2c6ab27f28bfa9ac
Andrew Forward <aforward@hey.com>
2023-05-17 14:24:42 -0400

    Add do nothing entry page

--- gamedrop/test/gamedrop_web/controllers/page_controller_test.exs
+++ gamedrop/test/gamedrop_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule GamedropWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Gamedrop Tavern"
   end
 end
