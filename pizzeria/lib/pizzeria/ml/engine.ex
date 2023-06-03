4c714bc37df6201addbd39e1fe7f886bf911c415
Andrew Forward <aforward@hey.com>
2023-06-03 15:41:29 -0400

    Filter out dates withour pizza dough numbers

--- pizzeria/lib/pizzeria/ml/engine.ex
+++ pizzeria/lib/pizzeria/ml/engine.ex
@@ -1,5 +1,6 @@
 defmodule Pizzeria.Ml.Engine do
   alias Pizzeria.Analytics
+  alias Pizzeria.Ml.Predictor, as: ML
   alias Scholar.Linear.LinearRegression, as: LR
 
   def train() do
@@ -10,8 +11,8 @@ defmodule Pizzeria.Ml.Engine do
 
     Analytics.reservations_by_date()
     |> Enum.reject(&Enum.member?(["Friday", "Saturday"], &1.day_of_week))
-    # For simplicity of demo assuming pizza doughs for every reservation date
     |> Enum.map(&[&1.num, pizza_doughs[&1.date]])
+    |> Enum.reject(fn [_num_resi, num_pizza] -> is_nil(num_pizza) end)
     |> Nx.tensor()
     |> then(fn data ->
       features = data[[.., 0..0]]
@@ -20,8 +21,10 @@ defmodule Pizzeria.Ml.Engine do
     end)
   end
 
-  def save_model(path) do
-    train()
+  def save_model(), do: save_model(train(), ML.model_filename())
+
+  def save_model(model, path) do
+    model
     |> Nx.serialize()
     |> then(&File.write!(path, &1))
   end
