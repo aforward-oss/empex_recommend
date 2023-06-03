f524f517f866d2a1062f194116ec49b2df202ab0
Andrew Forward <aforward@hey.com>
2023-06-03 16:06:30 -0400

    Add ?date=xxxx support to actually calculate estimated pizza doughs

--- pizzeria/lib/pizzeria_web/controllers/page_controller.ex
+++ pizzeria/lib/pizzeria_web/controllers/page_controller.ex
@@ -1,11 +1,10 @@
 defmodule PizzeriaWeb.PageController do
   use PizzeriaWeb, :controller
-  alias Pizzeria.Analytics
+  alias Pizzeria.{Analytics, Pos}
   alias Pizzeria.Ml.Predictor, as: ML
 
-  def home(conn, _params) do
-    # hack this for demo purposes
-    num_resis = :rand.uniform(30) + 19
+  def home(conn, params) do
+    num_resis = num_resis(params)
 
     conn
     |> assign(:num_resis_tonight, num_resis)
@@ -19,4 +18,7 @@ defmodule PizzeriaWeb.PageController do
     |> assign(:pizza_doughs_by_date, Analytics.pizza_doughs_by_date())
     |> render(:analytics)
   end
+
+  defp num_resis(%{"date" => date}), do: Pos.num_reservations_on(date)
+  defp num_resis(_params), do: Pos.num_reservations_tonight()
 end
