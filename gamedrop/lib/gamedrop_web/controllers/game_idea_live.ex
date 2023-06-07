0c3906c1b0b8bd7152564896602e2143ff94f7b4
Andrew Forward <aforward@hey.com>
2023-06-07 11:56:09 -0400

    Show the criteria

--- gamedrop/lib/gamedrop_web/controllers/game_idea_live.ex
+++ gamedrop/lib/gamedrop_web/controllers/game_idea_live.ex
@@ -156,12 +156,18 @@ defmodule GamedropWeb.GameIdeaLive do
 
               <div class="relative px-6 pt-0 pb-10 sm:py-40 lg:px-8 lg:py-56 lg:pr-0 text-right">
                 <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
-                  <h2 class="text-xl font-bold tracking-tight text-gray-400 sm:text-2xl">
+                  <h2 class="text-xl font-bold tracking-tight text-gray-600 sm:text-2xl">
                     Gamedrop Tavern recommends
                   </h2>
                   <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                     <%= @game %>
                   </h1>
+
+                  <p class="mt-4 text-lg text-gray-600">
+                    Based on <%= budget_label(assigns[:criteria]) %> <%= gameplay_types_label(
+                      assigns[:criteria]
+                    ) %>
+                  </p>
 
                   <%= if @more_games do %>
                     <button
@@ -210,6 +216,7 @@ defmodule GamedropWeb.GameIdeaLive do
 
     socket
     |> assign(:step, :calculating)
+    |> assign(:criteria, params)
     |> reply(:noreply)
   end
 
@@ -283,6 +290,23 @@ defmodule GamedropWeb.GameIdeaLive do
 
   def game_image_src(game_name) do
     "/images/games/#{Game.slug(%{game_name: game_name})}.jpg"
+  end
+
+  defp budget_label(criteria) do
+    case Map.get(criteria || %{}, "budget") do
+      nil -> "no budget preference"
+      "1" -> "a budget-friendly"
+      "2" -> "a standard"
+      "3" -> "a premium"
+    end
+  end
+
+  defp gameplay_types_label(criteria) do
+    case Map.get(criteria || %{}, "game_types") do
+      nil -> "game"
+      [one_type] -> "#{one_type} game"
+      many -> "#{Enum.join(many, ", ") |> String.downcase()} game"
+    end
   end
 
   defp reply(socket, ok), do: {ok, socket}
